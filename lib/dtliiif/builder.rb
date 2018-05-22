e6cfd08a4b140cfc7901469d4e30e7c3f659721f
Adam Jazairi <adamjazairi@gmail.com>
2018-05-22 16:27:45 -0400

    Add builder methods

--- lib/dtliiif/builder.rb
+++ lib/dtliiif/builder.rb
@@ -3,26 +3,104 @@ require 'dtliiif/digital_entity'
 
 module Dtliiif
   class Builder
-    def initialize
-      digital_entity_file = Dtliiif::DigitalEntity.new
+    def initialize(iiif_host, manifest_host, image_filetype)
+      @iiif_host = iiif_host
+      @manifest_host = manifest_host
+      @image_filetype = image_filetype
     end
 
-    def build
+    def build(digital_entity_path, descmd, structmap, sequence_div, component_div)
+      digital_entity_file = Dtliiif::DigitalEntity.new(digital_entity_path, descmd)
+
+      @sequence_base = "#{@iiif_host}/#{digital_entity_file.obj_id}"
+
+      sequence = IIIF::Presentation::Sequence.new
+      # TODO: Update sequence canvas generator for dtl files
+      # sequence.canvases = mets_file.struct_map.map.with_index { |comp, i| image_annotation_from_id("#{comp}.#{@image_filetype}", "#{comp}", i) }
+
+      range = IIIF::Presentation::Range.new
+      # TODO: Update sequence canvas generator for dtl files
+      # range.ranges = mets_file.struct_map.map.with_index { |comp, i| build_range("#{comp}.#{@image_filetype}", "#{comp}", i) }
+
+      manifest = build_manifest(mets_file)
+      manifest.sequences << sequence
+      manifest.structures << range
+      thumb = sequence.canvases.first.images.first.resource['@id'].gsub(/full\/full/, 'full/!200,200')
+      manifest.insert_after(existing_key: 'label', new_key: 'thumbnail', value: thumb)
+
+      structures = manifest["structures"][0]["ranges"]
+      manifest["structures"] = structures
+      manifest.to_json(pretty: true)
     end
 
     def build_manifest(digital_entity_file)
+      seed = {
+          '@id' => "#{@manifest_host}/#{digital_entity_file.obj_id}.json",
+          'label' => "#{digital_entity_file.label}",
+          'viewing_hint' => 'paged',
+          'attribution' => "#{digital_entity_file.rights_information}",
+          'metadata' => [
+            {"handle": "#{digital_entity_file.handle}"},
+            {"label": "Preferred Citation", "value": ""}
+          ]
+      }
+      IIIF::Presentation::Manifest.new(seed)
     end
 
     def image_annotation_from_id(image_file, label, order)
+      separator = image_file.include?('_') ? '_' : '.'
+      image_id = image_file.chomp('.jp2').chomp('.tif').chomp('.tiff').chomp('.jpg')
+      page_id = image_id.split(separator).last
+
+      canvas_id = "#{@sequence_base}/canvas/#{page_id}"
+
+      seed = {
+          '@id' => "#{canvas_id}/annotation/1",
+          'on' => canvas_id
+      }
+      annotation = IIIF::Presentation::Annotation.new(seed)
+      annotation.resource = image_resource_from_page_hash(image_file)
+
+      build_canvas(annotation, canvas_id, label)
     end
 
     def build_canvas(annotation, canvas_id, label)
+      seed = {
+          '@id' => canvas_id,
+          'label' => label,
+          'width' => annotation.resource['width'],
+          'height' => annotation.resource['height'],
+          'images' => [annotation]
+      }
+      IIIF::Presentation::Canvas.new(seed)
     end
 
     def build_range(image_file, label, order)
+      separator = image_file.include?('_') ? '_' : '.'
+      image_id = image_file.chomp('.jp2').chomp('.tif').chomp('.tiff').chomp('.jpg')
+      page_id = image_id.split(separator).last
+
+      range_id = "#{@sequence_base}/range/r-#{order}"
+      canvas_id = "#{@sequence_base}/canvas/#{page_id}"
+
+      seed = {
+          '@id' => range_id,
+          'label' => label,
+          'canvases' => [canvas_id]
+      }
+      IIIF::Presentation::Range.new(seed)
     end
 
     def image_resource_from_page_hash(page_id)
+      base_uri = "#{@iiif_host}/#{page_id}"
+      image_api_params = '/full/full/0/default.jpg'
+
+      params = {
+          service_id: base_uri,
+          resource_id_default: "#{base_uri}#{image_api_params}",
+          resource_id: "#{base_uri}#{image_api_params}"
+      }
+      IIIF::Presentation::ImageResource.create_image_api_image_resource(params)
     end
   end
 end
