d16427ca35b987e088bdd3bfee8beabff26ae719
Adam Jazairi <adamjazairi@gmail.com>
2018-05-24 09:13:40 -0400

    Fix preferred citation display

--- lib/dtliiif/builder.rb
+++ lib/dtliiif/builder.rb
@@ -39,7 +39,7 @@ module Dtliiif
           'attribution' => "#{digital_entity_file.rights_information}",
           'metadata' => [
             {"handle": "#{digital_entity_file.handle}"},
-            {"label": "Preferred Citation", "value": "#{digital_entity_file.label}, #{digital_entity_file.collection_name}, #{digital_entity_file.collection_id}, #{digital_entity_file.owner}, #{digital_entity_file.handle}.}"}
+            {"label": "Preferred Citation", "value": "#{digital_entity_file.label}, #{digital_entity_file.collection_name}, #{digital_entity_file.collection_id}, #{digital_entity_file.owner}, #{digital_entity_file.handle}."}
           ]
       }
       IIIF::Presentation::Manifest.new(seed)
